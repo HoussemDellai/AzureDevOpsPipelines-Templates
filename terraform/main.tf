6a7a41905425cd2250fe39378d42ba9a8a96d24d
Houssem Dellai <houssem.dellai@live.com>
2020-06-26 23:35:45 +0200

    Create main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,14 @@
+provider "azurerm" {
+  version = ">=2.0"
+  # The "feature" block is required for AzureRM provider 2.x.
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource-group-name
+  location = "westeurope"
+  
+  tags = {
+    environment = "production"
+  }
+}
