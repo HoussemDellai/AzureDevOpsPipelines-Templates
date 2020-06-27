cddff3a3d08ef8c5164d9bbc03126979ee2e66f4
Houssem Dellai <houssem.dellai@live.com>
2020-06-27 07:49:38 +0200

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,3 +1,8 @@
+variable "environment" {
+  default = "development"
+  description = "The Environment name, i.e dev, staging, qa, preprod, test, prod..."
+}
+
 variable "resource-group-name" {
   default = "terraform-resource-group-test-tbd"
   description = "The Resource Group name to be deployed"
