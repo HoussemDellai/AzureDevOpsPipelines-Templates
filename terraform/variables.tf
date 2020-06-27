9f17da5edbe79fc8347809167ecd15cc0777493c
Houssem Dellai <houssem.dellai@live.com>
2020-06-27 08:14:06 +0200

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -4,6 +4,6 @@ variable "environment" {
 }
 
 variable "resource-group-name" {
-  default = "resource-group-dev"
+  default = "resourcegroup-dev"
   description = "The Resource Group name to be deployed"
 }
